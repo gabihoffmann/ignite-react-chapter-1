5ca8546378c0fed55e0cea2337b2d2c828ff5913
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-03-29 22:00:52 -0300

    create the 1st component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import RepositoryList from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
-  return <h1>Boa noite React ⚠️</h1>
+  return <RepositoryList/>
 }
